e71ec462aaa614616e4c9f8c3f44854ad64eedb8
Ivan Lara <ukikalix@gmail.com>
2025-03-14 17:33:37 -0400

    ft/empty games state added

--- src/components/features/games/grid/index.tsx
+++ src/components/features/games/grid/index.tsx
@@ -15,6 +15,12 @@ export const GamesGrid = ({ genre }: Props) => {
 
     return (
         <section className="w-full flex flex-col gap-y-12">
+            {games.length === 0 && (
+                <div className="w-full flex justify-center items-center py-12 border-card/50 rounded-lg">
+                    <p className="text-foreground">No games found</p>
+                </div>
+            )}
+
             <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-12">
                 {games.map((game: Game) => {
                     return <GameCard key={game.id} game={game} />;
